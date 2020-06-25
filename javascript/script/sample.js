cd2d307744cba2e2f4c8038088c7f912173ea293
youhyunwoo <dbgusdn0922@naver.com>
2020-06-25 13:23:44 +0900

    add sample

---
+++ javascript/script/sample.js
@@ -0,0 +1,25 @@
+var Sample = {
+
+	name: "Sample",
+	desc: "Sample",
+
+	asset: {
+	},
+
+	scene: {
+		title: {
+			in() {
+				
+			},
+			out() {
+				
+			},
+			update(delta) {
+				
+			},
+			draw(context) {
+				
+			}
+		},
+	}
+};
