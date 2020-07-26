0de9a4476ec396a744089565dd4e346dc4811310
Stop <dbgusdn0922@naver.com>
2020-07-27 01:39:36 +0900

    add normal, distance in raycast result

--- javascript/script/util/ray.js
+++ javascript/script/util/ray.js
@@ -1,7 +1,9 @@
 import * as Vector from '/script/util/vector.js';
 
 export function Raycast(start, end, object) {
-	const [l, t, r, b] = [...object.transform.position, ...object.transform.position.addv(object.transform.size)];
+	// objects: center anchor
+
+	const [l, t, r, b] = object.ltrb;
 
 	const [p, q] = [start, end];
 	const [pqx, pqy] = q.subv(p);
@@ -29,16 +31,26 @@ export function Raycast(start, end, object) {
 
 	const rates = [txf, txl, tyf, tyl];
 	const negatives = rates.filter(x => x < 0).length;
-	const front = negatives < 2;
-	const reach = rates.filter(x => x >= 1).length < 3;
+	const front = negatives < 2; // Math.floor(rates.length + 1) / 2)
+	const reach = rates.filter(x => x >= 1).length < 3; // Math.ceil(rates.length + 1) / 2)
 	const collide = (xf[0] - yl[0]) * (yf[0] - xl[0]) > 0 && front && reach;
 	const point = collide ? (xf[0] < yf[0] ? yf.slice(1) : xf.slice(1)) : null;
 	const normal = collide ? [(xf[0] < yf[0] ? 0 : 1) * (xfl ? -1 : 1), (xf[0] < yf[0] ? 1 : 0) * (yfl ? -1 : 1)] : null;
 	// const inner = (xf[0] - yl[0]) * (yf[0] - xl[0]) > 0 && negatives == 2;
+	const distanceSquare = collide ? point.subv(start).dotv(point.subv(start)) : null;
+	const distance = collide ? Math.sqrt(distanceSquare) : null;
 
 	return {
 		hit: collide,
 		point: point,
-		normal: normal
+		normal: normal,
+		distance: distance,
+		distanceSquare: distanceSquare,
 	};
+}
+
+export function Boxcast(size, start, end, object) {
+	const enlarged = object.enlarge(...size);
+
+	return Raycast(start, end, enlarged);
 }
